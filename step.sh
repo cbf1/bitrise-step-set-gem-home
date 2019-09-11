21fc98e3557974697b94916b9c3be81b3ea8e9ef
Christopher Fuller <chris@chrisfuller.me>
2019-09-11 08:18:39 -0700

    Set GEM_CACHE_PATH instead of GEM_HOME

--- step.sh
+++ step.sh
@@ -2,4 +2,7 @@
 
 set -euxo pipefail
 
-envman add --key GEM_HOME --value "$(gem environment gemdir)"
+RUBY_PATH=$(rbenv which ruby)
+GEM_CACHE_PATH=$(cd "${RUBY_PATH%/bin/ruby}"; pwd)
+
+envman add --key GEM_CACHE_PATH --value "${GEM_CACHE_PATH}"
